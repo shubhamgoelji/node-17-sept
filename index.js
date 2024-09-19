16508488437b258546191945b4aaf0ea43759c87
shubhamgoelji <shubhamgoelji@gmail.com>
2024-09-19 14:16:44 +0530

    updation.....

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connect = require('./connection');
+const student = require('./routes/student');
+const app = express();
+connect();
+app.use(express.json());
+app.use(student);
+
+
+//localhost:3000/add/user
+app.listen(3000, (err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("server is running on 3000")
+    }
+})
